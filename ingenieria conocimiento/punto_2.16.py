01c800941188a50f9ab43490e9572f09bbde900f
Gianluca Peruzzi <gianlucaperuzzi@MacBook-Air-de-Gianluca.local>
2026-08-14 20:09:53 -0300

    tp - 2.16

---
+++ ingenieria conocimiento/punto_2.16.py
@@ -0,0 +1,17 @@
+# ejercicio 2.16
+
+def crear_perfil(**kwargs):
+    
+    return kwargs
+
+
+# prueba del codigo
+
+perfil_juan = crear_perfil(
+    nombre="Luis",
+    edad=25,
+    email= "juan@mail.com",
+    ciudad="madrid" 
+)
+
+print("Perfil de Luis:", perfil_juan)
